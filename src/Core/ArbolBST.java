30d5ccdcaa4ecf9ac16869be2b8961ee60d40cb8
Eliudqa <aycardie@uninorte.edu.co>
2025-09-03 21:19:14 -0500

    Adicion metodos de obtener inventario, maximo, minimo y 2 eventos de juego

--- src/Core/ArbolBST.java
+++ src/Core/ArbolBST.java
@@ -4,40 +4,45 @@
  */
 package Core;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author samit
  */
 public class ArbolBST {
+
     Nodo raiz;
+    ArrayList<Nodo> inventario;
 
     public ArbolBST(Nodo raiz) {
         this.raiz = raiz;
+        this.inventario = new ArrayList<>();
     }
-    
-    public void insertarGema(Nodo gema, Nodo actual){
-        if(this.raiz == null){
+
+    public void insertarGema(Nodo gema, Nodo actual) {
+        if (this.raiz == null) {
             this.raiz = gema;
-           }
-        else if(gema.getPoder()> actual.getPoder() ){
-            if(actual.getLd() != null){
+        } else if (gema.getPoder() > actual.getPoder()) {
+            if (actual.getLd() != null) {
                 insertarGema(gema, actual.getLd());
-            }else{
-            actual.setLd(gema);}
-        }else if(gema.getPoder()< actual.getPoder()){
-            if(actual.getLi() != null){
+            } else {
+                actual.setLd(gema);
+            }
+        } else if (gema.getPoder() < actual.getPoder()) {
+            if (actual.getLi() != null) {
                 insertarGema(gema, actual.getLi());
-            }else{
-            actual.setLi(gema);}
-        }else if(gema.getPoder()== actual.getPoder()){
+            } else {
+                actual.setLi(gema);
+            }
+        } else if (gema.getPoder() == actual.getPoder()) {
             System.out.println("¡No puedes llevar 2 gemas del mismo poder!");
         }
     }
-    
-    
-    private Nodo buscarRecursivo(Nodo actual, int clave) {
+
+    public Nodo buscarRecursivo(Nodo actual, int clave) {
         if (actual == null) {
-            return null; 
+            return null;
         }
         if (clave == actual.getPoder()) {
             return actual;
@@ -48,8 +53,8 @@ public class ArbolBST {
             return buscarRecursivo(actual.getLd(), clave);
         }
     }
-    
-    private Nodo eliminarRecursivo(Nodo actual, int clave) {
+
+    public Nodo eliminarRecursivo(Nodo actual, int clave) {
         if (actual == null) {
             return null;
         }
@@ -63,50 +68,55 @@ public class ArbolBST {
             // Caso 1: sin hijos
             if (actual.getLi() == null && actual.getLd() == null) {
                 return null;
-            }
-            // Caso 2: un solo hijo
+            } // Caso 2: un solo hijo
             else if (actual.getLi() == null) {
                 return actual.getLd();
             } else if (actual.getLd() == null) {
                 return actual.getLi();
-            }
-            // Caso 3: dos hijos
+            } // Caso 3: dos hijos
             else {
                 Nodo sucesor = encontrarSucesor(actual);
-                sucesor.setLi(actual.getLi());
-                sucesor.setLd(actual.getLd());
-                sucesor.setLr(null);
-                actual = sucesor;
-                eliminarRecursivo(actual.getLd(), sucesor.getPoder());
+                actual.poder = sucesor.poder;
+                actual.nombre = sucesor.nombre;
+                actual.coordX = sucesor.coordX;
+                actual.coordY = sucesor.coordY;
+                actual.setLd(eliminarRecursivo(actual.getLd(), sucesor.getPoder()));
+
             }
         }
         return actual;
     }
-    
-    public void inOrden(Nodo actual) {
+
+    public ArrayList obtenerInventario() {
+        ObtenerinOrden(raiz);
+        return this.inventario;
+    }
+
+    public void ObtenerinOrden(Nodo actual) {
         if (actual != null) {
-            inOrden(actual.getLi());
-            System.out.print(actual.getPoder() + " ");
-            inOrden(actual.getLd());
+            inventario.clear();
+            ObtenerinOrden(actual.getLi());
+            this.inventario.add(actual);
+            ObtenerinOrden(actual.getLd());
         }
     }
-    
+
     public void preOrden(Nodo actual) {
         if (actual != null) {
             System.out.print(actual.getPoder() + " ");
-            preOrden(actual.getLi());            
+            preOrden(actual.getLi());
             preOrden(actual.getLd());
         }
     }
-    
+
     public void postOrden(Nodo actual) {
         if (actual != null) {
-            postOrden(actual.getLi());            
+            postOrden(actual.getLi());
             postOrden(actual.getLd());
             System.out.print(actual.getPoder() + " ");
         }
     }
-    
+
     public Nodo encontrarSucesor(Nodo nodo) {
         Nodo sucesor = nodo.getLd();
         while (sucesor.getLi() != null) {
@@ -114,7 +124,22 @@ public class ArbolBST {
         }
         return sucesor;
     }
-    
+
+    public Nodo encontrarSucesor(int clave) {
+        Nodo actual = raiz;
+        Nodo sucesor = null;
+
+        while (actual != null) {
+            if (clave < actual.getPoder()) {
+                sucesor = actual; // candidato a sucesor
+                actual = actual.getLi();
+            } else {
+                actual = actual.getLd();
+            }
+        }
+        return sucesor;
+    }
+
     public Nodo encontrarPredecesor(Nodo nodo) {
         Nodo predecesor = nodo.getLi();
         while (predecesor.getLd() != null) {
@@ -122,4 +147,36 @@ public class ArbolBST {
         }
         return predecesor;
     }
+
+    public Nodo encontrarPredecesor(int clave) {
+        Nodo actual = raiz;
+        Nodo predecesor = null;
+
+        while (actual != null) {
+            if (clave > actual.getPoder()) {
+                predecesor = actual; // candidato a predecesor
+                actual = actual.getLd();
+            } else {
+                actual = actual.getLi();
+            }
+        }
+
+        return predecesor;
+    }
+    
+    public Nodo encontrarMinimo() {
+    Nodo actual = raiz;
+    while (actual != null && actual.getLi() != null) {
+        actual = actual.getLi();
+    }
+    return actual;
+}
+
+public Nodo encontrarMaximo() {
+    Nodo actual = raiz;
+    while (actual != null && actual.getLd() != null) {
+        actual = actual.getLd();
+    }
+    return actual;
+}
 }
--- src/Core/Juego.java
+++ src/Core/Juego.java
@@ -4,44 +4,76 @@
  */
 package Core;
 
-/**
- *
- * @author HOLA
- */
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Juego {
 
+    private Random random;
+    private ArbolBST arbol;
+
     public Juego() {
+        this.random = new Random();
+        this.arbol = new ArbolBST(null);
     }
-    
+
     public enum EnumEventos {
-    JEFEENEMIGO,COFRES,PORTAL,TRAMPA
-}
-    
-    public void ejecutarEvento(EnumEventos evento){
-        switch(evento){
+        JEFEENEMIGO, COFRES, PORTAL, TRAMPA
+    }
+
+    public void ejecutarEvento(EnumEventos evento) {
+        switch (evento) {
             case JEFEENEMIGO:
-                jefeEnemigo();
+                jefeEnemigo(random.nextInt(100));
             case COFRES:
                 cofre();
             case PORTAL:
                 abrirPortal();
             case TRAMPA:
                 trampa();
-                
+
         }
     }
-    
-    public void jefeEnemigo(int gemaPedir){
+
+    public Nodo jefeEnemigo(int gemaPedir) {
+        Nodo raiz = arbol.raiz;
+        Nodo gemaBuscada = this.arbol.buscarRecursivo(raiz, gemaPedir);
+
+        if (gemaBuscada != null) {
+            return gemaBuscada;
+        } else {
+            
+            Nodo sucesor = this.arbol.encontrarSucesor(gemaPedir);
+            Nodo predecesor = this.arbol.encontrarPredecesor(gemaPedir);
+
+            if (sucesor != null) {
+                return sucesor;
+            } else if (predecesor != null) {
+                return predecesor;
+            } else {
+                return null; // El árbol está vacío
+            }
+        }
+
+    }
+
+    public void cofre() {
+        
+
         
     }
-    public void cofre(){
-        
+
+    public void abrirPortal() {
+
     }
-    public void abrirPortal(){
-        
+
+    public void trampa() {
+
+     ArrayList<Nodo> inventario = this.arbol.obtenerInventario();
+     int index = random.nextInt(inventario.size());
+     Nodo gemaAEliminar = inventario.get(index);
+     arbol.eliminarRecursivo(gemaAEliminar, gemaAEliminar.getPoder());
+               
     }
-    public void trampa(){
-        
-    }
-    
+
 }
